af95759707ee64b1858c4ccb72928e875a2c4980
jsmchauhan <chauhansmanoj@gmail.com>
2017-07-09 18:39:15 +0100

    added tutorial 2 files

--- java/corejava/src/tutorial2/Application.java
+++ java/corejava/src/tutorial2/Application.java
@@ -8,4 +8,29 @@ public class Application {
 
 
 
+    public static void main(String[] args){
+
+        int myNumber=10;
+        long myLong=1231231;
+        short myShort=12;
+
+        double myDouble= 7.23;
+        float myFloat=12.4f;
+
+        char myByte='3';
+        Boolean myBoolean = false;
+
+        byte myBytte=123;
+
+        System.out.println("myNumber: " + myNumber);
+        System.out.println("myLong: " + myLong);
+        System.out.println("myShort: " + myShort);
+        System.out.println("myDouble: " + myDouble);
+        System.out.println("myFloat: " + myFloat);
+        System.out.println("myBoolean: " + myBoolean);
+        System.out.println("myByte: " + myByte);
+        System.out.println("myByyte: " + myBytte);
+
+    }
+
 }
